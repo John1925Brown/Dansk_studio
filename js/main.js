00d6a95568b02ccba48e86485a6a26a2f3659f57
John1925Brown <dima.yaroshevskiy25@mail.ru>
2022-06-13 01:19:33 +0300

    all docs
    feat: added burger menu

--- js/main.js
+++ js/main.js
@@ -42,3 +42,34 @@ sliders.forEach((el) => {
   next.css('left', prev.width() + 20 + fraction.width() + 30)
   fraction.css('left', prev.width() + 30)
 });
+
+$('.header__burger').on('click', function (e) {
+  e.preventDefault;
+  $(this).toggleClass('header__burger--active');
+  $('.menu__nav').toggleClass('menu__nav--active');
+  $('.friends').toggleClass('friends--active');
+});
+
+(function () {
+  const header = document.querySelector('.header')
+  window.onscroll = () => {
+    if(window.pageYOffset > 50) {
+      header.classList.add('header__scroll');
+    }
+      else{
+        header.classList.remove('header__scroll')
+      }
+  };
+  }());
+
+
+
+
+
+
+/*
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.menu__nav').toggleClass('active');
+  });
+}); */
